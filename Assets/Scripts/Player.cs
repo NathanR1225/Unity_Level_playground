a7260d8e8eb3bb9fb1f3a60b862e5217d891da61
NathanFireFly <NathanR@fireflyint.com>
2020-10-06 20:36:38 -0400

    Player can now move. Also messed with the camera a bit.
    The player can now move in all 4 directions and can sprint. Started to mess with the camera but the camera pivots instead of orbiting the player.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rigidbodyComponet;
+    private float horizontalInput = 0;
+    private float verticalInput = 0;
+    private bool sprint = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponet = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprint = (Input.GetKey(KeyCode.LeftShift)|| Input.GetKey(KeyCode.RightShift));
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        float speed = sprint ? 10 : 5;
+        rigidbodyComponet.velocity = new Vector3(horizontalInput* speed, rigidbodyComponet.velocity.y, verticalInput* speed);
+        Debug.Log("speed " + sprint);
+    }
+}
